9a9682abefc4c29fa54d669256bfb815c6697634
Sanket Vetkoli <vetkolisanket@gmail.com>
2019-07-12 20:20:27 +0530

    Added NotesRepository and populated db with dummy notes while creating

--- app/src/main/java/com/sanket/todoapp/NoteDatabase.kt
+++ app/src/main/java/com/sanket/todoapp/NoteDatabase.kt
@@ -1,9 +1,11 @@
 package com.sanket.todoapp
 
 import android.content.Context
+import android.os.AsyncTask
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 /**
  * Created by Sanket on 2019-07-12.
@@ -17,14 +19,37 @@ abstract class NoteDatabase : RoomDatabase() {
 
         private var instance: NoteDatabase? = null
 
+        @JvmStatic
         fun getInstance(context: Context): NoteDatabase {
             if (instance == null) {
                 instance = Room.databaseBuilder(context.applicationContext, NoteDatabase::class.java, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build()
             }
             return instance as NoteDatabase
         }
+
+        val roomCallback = object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                PopulateDbAsyncTask(instance!!).execute()
+            }
+        }
+
+        class PopulateDbAsyncTask(db: NoteDatabase) : AsyncTask<Void, Void, Void>() {
+
+            private val noteDao by lazy { db.noteDao() }
+
+
+            override fun doInBackground(vararg params: Void?): Void? {
+                noteDao.insert(Note("Title 1", "Description 1", 1))
+                noteDao.insert(Note("Title 2", "Description 2", 2))
+                noteDao.insert(Note("Title 3", "Description 2", 3))
+                return null
+            }
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/sanket/todoapp/NotesRepository.kt
@@ -0,0 +1,72 @@
+package com.sanket.todoapp
+
+import android.app.Application
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+
+/**
+ * Created by Sanket on 2019-07-12.
+ */
+class NotesRepository(application: Application) {
+
+    private val noteDao: NoteDao
+    val allNotes: LiveData<List<Note>>
+
+    init {
+        val database = NoteDatabase.getInstance(application)
+        noteDao = database.noteDao()
+        allNotes = noteDao.getAllNotes()
+    }
+
+    fun insert(note: Note) {
+        InsertNoteAsyncTask(noteDao).execute(note)
+    }
+
+    fun update(note: Note) {
+        UpdateNoteAsyncTask(noteDao).execute(note)
+    }
+
+    fun delete(note: Note) {
+        DeleteNoteAsyncTask(noteDao).execute(note)
+    }
+
+    fun deleteAll() {
+        DeleteAllNoteAsyncTask(noteDao).execute()
+    }
+
+    companion object {
+        class InsertNoteAsyncTask(private val noteDao: NoteDao) : AsyncTask<Note, Void, Void>() {
+
+            override fun doInBackground(vararg notes: Note): Void? {
+                noteDao.insert(notes[0])
+                return null
+            }
+        }
+
+        class UpdateNoteAsyncTask(private val noteDao: NoteDao) : AsyncTask<Note, Void, Void>() {
+
+            override fun doInBackground(vararg notes: Note): Void? {
+                noteDao.update(notes[0])
+                return null
+            }
+        }
+
+        class DeleteNoteAsyncTask(private val noteDao: NoteDao) : AsyncTask<Note, Void, Void>() {
+
+            override fun doInBackground(vararg notes: Note): Void? {
+                noteDao.delete(notes[0])
+                return null
+            }
+        }
+
+        class DeleteAllNoteAsyncTask(private val noteDao: NoteDao) : AsyncTask<Void, Void, Void>() {
+
+            override fun doInBackground(vararg voids: Void): Void? {
+                noteDao.deleteAll()
+                return null
+            }
+        }
+    }
+
+
+}
